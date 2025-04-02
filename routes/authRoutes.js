98381c73991e87f8ebe104be9027b97807a17b48
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2025-04-02 13:13:19 +0530

    Update authRoutes.js
    updated login rout at root

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const router = express.Router();
-
+router.get('/', authController.login);
 router.get('/login', authController.login);
 router.post('/login', authController.authenticate);
 router.get('/logout', authController.logout);
