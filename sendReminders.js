8fba926b6af5921d366383d0a5e1a326ec200549
yashika181007 <166136345+yashika181007@users.noreply.github.com>
2025-04-02 15:21:21 +0530

    Update sendReminders.js

--- sendReminders.js
+++ sendReminders.js
@@ -55,7 +55,8 @@ const sendReminderSMS = async () => {
 };
 
 // Schedule the job to run every day at 9 AM
-cron.schedule('20 15 * * *', sendReminderSMS);
+cron.schedule('25 15 * * *', sendReminderSMS);
+
 
 
 console.log("⏳ Reminder scheduler started...");
